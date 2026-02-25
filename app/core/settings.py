a62b83c877fe9018dc2487febd2b2c95beb8e807
Timur Baybulatov <baibulatovtimur@mail.ru>
2026-02-25 21:39:10 +0500

    chore: add env example and settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from functools import lru_cache
 
 from pydantic import Field
@@ -8,9 +6,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        extra="ignore"
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
     bot_token: str = Field(..., alias="BOT_TOKEN")
 
